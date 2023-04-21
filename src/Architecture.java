7ad448ff33e0b0d0d6bbc24c9a0d846641552f1e
Anand <anandcherian.585@gmail.com>
2023-04-21 12:15:44 +0530

    Architect Y has made his changes

--- src/Architecture.java
+++ src/Architecture.java
@@ -3,4 +3,8 @@ public class Architecture {
     public static void printX() {
         System.out.println("Print method of Architect X");
     }
+
+    public static void printY() {
+        System.out.println("Print method of Architect Y");
+    }
 }
